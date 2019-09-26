5002ffca66cc2e0b7ab09c9e80642ab31f58dc84
Sergio Flores Ramírez <serchfr890@outlook.es>
2019-09-26 14:08:06 -0500

    Se agregan los modelos de datos del proyecto

---
+++ NoticiasApiProject/NoticiasApiProject/Models/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasApiProject.Models
+{
+    public class Autor
+    {
+        public int AutorID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
---
+++ NoticiasApiProject/NoticiasApiProject/Models/Noticia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasApiProject.Models
+{
+    public class Noticia
+    {
+        public int NoticiaID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; }
+        public int AutorId { get; set; }
+    }
+}
